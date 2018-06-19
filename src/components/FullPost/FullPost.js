b729b6b58fd6e1150a89ace4b0ae33227038c549
arthur <arthur@worldcomfinance.com>
2018-06-19 17:38:32 +0400

    ajax request

--- src/components/FullPost/FullPost.js
+++ src/components/FullPost/FullPost.js
@@ -1,20 +1,64 @@
 import React, { Component } from 'react';
+import axiosInstance from '../../axios';
 
 import './FullPost.css';
 
 class FullPost extends Component {
-    render () {
-        let post = <p>Please select a Post!</p>;
-        post = (
-            <div className="FullPost">
-                <h1>Title</h1>
-                <p>Content</p>
-                <div className="Edit">
-                    <button className="Delete">Delete</button>
-                </div>
-            </div>
+    state = {
+        loadedPost: null
+    };
 
-        );
+    async componentDidUpdateCall() {
+        try {
+            let data = await axiosInstance.get(`/posts/${this.props.id}`);
+            this.setState(
+                {
+                    loadedPost: data.data
+                }
+            );
+        } catch (e) {
+            console.log(e);
+        }
+    }
+
+    componentDidUpdate () {
+        if (this.props.id) {
+            if (!this.state.loadedPost || (this.state.loadedPost && this.state.loadedPost.id !== this.props.id)) {
+                this.componentDidUpdateCall();
+            }
+        }
+    }
+
+    deletePostHandler = () => {
+        this.deletePostHandlerCall();
+    };
+
+    deletePostHandlerCall = async () => {
+        try {
+            let deleted = await axiosInstance.delete(`/posts/${this.props.id}`);
+            console.log(deleted);
+        } catch (e) {
+            console.log(e);
+        }
+    };
+
+    render () {
+        let post = <p style={{textAlign: "center"}}>Please select a Post!</p>;
+
+        if (this.state.loadedPost) {
+            post = (
+                <div className="FullPost">
+                    <h1>{this.state.loadedPost.title}</h1>
+                    <p>{this.state.loadedPost.body}</p>
+                    <div className="Edit">
+                        <button className="Delete" onClick={this.deletePostHandler}>Delete</button>
+                    </div>
+                </div>
+
+            )
+        }
+
+        post = (post);
         return post;
     }
 }
